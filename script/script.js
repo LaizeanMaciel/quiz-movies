431c8df2c4f18ddf6840255561f5661b2bd15d06
laizean <laizeanmaciel20@gmail.com>
2024-04-30 16:55:57 -0300

    Add modal

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+/* --------------- Lista de filmes --------------- */
+
 const movies = [
     {
         titulo: "Sexta-Feira 13",
@@ -60,6 +62,103 @@ const movies = [
    
   ];
 
+  /*--------------- Perguntas --------------------*/
+
+  const perguntas = [
+    {
+        pergunta: "Quem foi o diretor do filme 'Sexta-Feira 13' de 1980?",
+        respostas: [
+            "Wes Craven",
+            "Tobe Hooper",
+            "Sean S. Cunningham"
+        ],
+        correta: 2
+    },
+    {
+        pergunta: "Onde se passa a maior parte da ação do filme?",
+        respostas: [
+            "Numa cidade pequena",
+            "Numa ilha deserta",
+            "Num acampamento de verão"
+        ],
+        correta: 2
+    },
+    {
+        pergunta: "Qual é o nome do assassino em 'Sexta-Feira 13'?",
+        respostas: [
+            "Michael Myers",
+            "Jason Voorhees",
+            "Freddy Krueger"
+        ],
+        correta: 1
+    },
+    {
+        pergunta: "Quem é a protagonista do filme?",
+        respostas: [
+            "Laurie Strode",
+            "Nancy Thompson",
+            "Alice Hardy"
+        ],
+        correta: 2
+    },
+    {
+        pergunta: "Qual é o motivo do assassino em 'Sexta-Feira 13'?",
+        respostas: [
+            "Vingança por uma injustiça sofrida",
+            "Loucura súbita",
+            "Adoração ao sobrenatural"
+        ],
+        correta: 0
+    },
+    {
+        pergunta: "Qual é o título original do filme?",
+        respostas: [
+            "Friday the 13th",
+            "Nightmare on Elm Street",
+            "Halloween"
+        ],
+        correta: 0
+    },
+    {
+        pergunta: "Quantos filmes da série 'Sexta-Feira 13' foram lançados até 2022?",
+        respostas: [
+            "3",
+            "10",
+            "12"
+        ],
+        correta: 2
+    },
+    {
+        pergunta: "Qual é o nome do acampamento onde ocorrem os assassinatos no filme?",
+        respostas: [
+            "Camp Crystal Lake",
+            "Camp Green Lake",
+            "Camp Firewood"
+        ],
+        correta: 0
+    },
+    {
+        pergunta: "Quem foi o responsável pelo roteiro do filme?",
+        respostas: [
+            "Wes Craven",
+            "John Carpenter",
+            "Victor Miller"
+        ],
+        correta: 2
+    },
+    {
+        pergunta: "Qual foi a principal influência para a criação do filme?",
+        respostas: [
+            "Halloween",
+            "A Noite dos Mortos-Vivos",
+            "O Massacre da Serra Elétrica"
+        ],
+        correta: 0
+    }
+  ];
+
+  /*------------------------------------------------------------------------*/
+
   const template = document.querySelector('template')
   const filmes = document.querySelector('#filmes')
   
@@ -74,3 +173,5 @@ const movies = [
     filmes.appendChild(movieItem)
     
   }
+
+  
